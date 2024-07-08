fix: invert depth calculation

object_distance_estimation/depth_fusion_node.py
@@ -21,15 +21,11 @@ class DepthFusionNode(Node):
 
     def lidar_callback(self, msg):
         data = np.array(msg.data).reshape((-1, 3))
-        
-        # print min max depth
-        #print(min(data[:, 2]), max(data[:, 2]))
-
         self.lidar_depth_map.fill(0)  # Reset the depth map
 
-        # Update the depth map with available LiDAR data
+        # Update the depth map with available LiDAR data, ignoring NaN values
         for x, y, depth in data:
-            if 0 <= int(x) < 640 and 0 <= int(y) < 480:
+            if 0 <= int(x) < self.image_width and 0 <= int(y) < self.image_height and not np.isnan(depth) and not np.isinf(depth):
                 self.lidar_depth_map[int(y), int(x)] = depth
 
         interpolate = True  # Set to True to interpolate missing depth values
@@ -100,9 +96,16 @@ class DepthFusionNode(Node):
                     # Calculate the relative depth difference between the current pixel's estimated depth and the reference estimated depth
                     relative_depth = estimated_column[y] - reference_estimated_depth
 
+                    # Instead of this calculation use a kalman filter to estimate the depth
                     # Adjust the depth of the current pixel based on the relative depth and the scale of the reference LiDAR depth
                     # This assumes a linear relationship between the change in estimated depth and actual depth
                     column[y] = reference_depth + (relative_depth * reference_depth / reference_estimated_depth)
+                    
+                    #if reference_estimated_depth != 0:
+                    #    scaling_factor = reference_depth / reference_estimated_depth
+                    #    column[y] = reference_depth + (relative_depth * scaling_factor)
+                    #else:
+                    #    column[y] = reference_depth  # Default to LiDAR depth if no valid estimated depth is available
 
             # Update the fused depth map with the adjusted column values
             fused_depth_map[:, x] = column

object_distance_estimation/image_distance_visualizer_node.py
@@ -35,14 +35,17 @@ class ImageDistanceVisualizer(Node):
     def distance_callback(self, msg):
         # Process the incoming distance data
         distance_data = np.array(msg.data, dtype=np.float32).reshape(-1, 3)
+
+        # distance_data[np.isnan(distance_data)] = 0
+        # distance_data[np.isinf(distance_data)] = 0
         self.current_distances = distance_data
         self.display_image_with_distances()
 
     def display_image_with_distances(self):
         if self.current_image is not None and self.current_distances is not None:
             # Get maximum distance for normalization
-            max_distance = np.max(self.current_distances[:, 2]) if self.current_distances[:, 2].size > 0 else 1
-            
+            max_distance = np.max(self.current_distances[:, 2]) if self.current_distances[:, 2].size > 0 else 1            
+
             # Overlay color on the image based on distance
             for point in self.current_distances:
                 x, y, distance = int(point[0]), int(point[1]), point[2]

object_distance_estimation/yolov5_node.py
@@ -27,7 +27,7 @@ class YOLOv5Node(Node):
         
         # Laden des YOLOv5-Modells von ultralytics
         # yolov5s = small Modell
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5l')  
 
     # Callback-Funktion, wenn eine neue Bildnachricht empfangen wird
     def listener_callback(self, msg):
@@ -39,11 +39,12 @@ class YOLOv5Node(Node):
         detections = []
         for result in results.xyxy[0].cpu().numpy():  
             x1, y1, x2, y2, conf, cls = result  
-            detections.append({
+            if conf > 0.45:  # Confidence threshold
+                detections.append({
                 'label': self.model.names[int(cls)],  # Klassifizierungsinformationen
                 'bbox': [int(x1), int(y1), int(x2), int(y2)],  # Bounding Box Koordinaten
                 'confidence': float(conf)  # Konfidenz der Erkennung
-            })
+                })
 
         self.publisher.publish(String(data=json.dumps(detections)))
 